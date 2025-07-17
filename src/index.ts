8f71654e71d392f9be044e5847a7096462769ff8
Blaze Burgess <blaze.i.burgess@gmail.com>
2025-07-17 18:24:10 -0400

    Export core modules for GDELT API client
    - Add exports for constants, API parameter interfaces, and API response interfaces.
    - Include the main client export to streamline access to the `GdeltClient` and related modules.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Happy developing ✨')
+/**
+ * GDELT API TypeScript Client
+ * A strongly-typed client for interacting with the GDELT API
+ */
+
+// Export constants
+export * from './constants';
+
+// Export interfaces
+export * from './interfaces/api-parameters';
+export * from './interfaces/api-responses';
+
+// Export client
+export * from './client';
